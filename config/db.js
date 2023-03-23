530aed187f13d9ae8fa0cdddfb9453178e6db516
Beeni <beeni4429@gmail.com>
2023-03-23 19:47:52 +0500

    change DatabaseURI

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-const MONGOURI = "mongodb://localhost:27017/Registration";
-// const MONGOURI = "mongodb+srv://mubeen:xwMHMxCMVqhEqkRe@cluster0.zaasaak.mongodb.net/Registration";
+// const MONGOURI = "mongodb://localhost:27017/Registration";
+const MONGOURI =
+  "mongodb+srv://mubeen:xwMHMxCMVqhEqkRe@cluster0.zaasaak.mongodb.net/Registration";
 
 const mongoServer = async () => {
   try {
